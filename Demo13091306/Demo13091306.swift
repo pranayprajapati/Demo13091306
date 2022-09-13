356f88ee2cc0e3e909c3bad79257c76314a02134
Leo <agskishor@gmail.com>
2022-09-13 15:31:02 +0530

    1530

---
+++ Demo13091306/Demo13091306.swift
@@ -0,0 +1,20 @@
+//
+//  Demo13091306.swift
+//  Demo13091306
+//
+//  Created by MAcBook on 13/09/22.
+//
+
+import Foundation
+
+public final class Demo13091306 {
+    let name = "Demo13091306"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- Demo13091306Tests/Demo13091306Tests.swift
+++ Demo13091306Tests/Demo13091306Tests.swift
@@ -9,15 +9,29 @@ import XCTest
 @testable import Demo13091306
 
 class Demo13091306Tests: XCTestCase {
-
+    
+    var demo13091306: Demo13091306!
+    
+    override func setUp() {
+        demo13091306 = Demo13091306()
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(demo13091306.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(demo13091306.sub(a: 2, b: 1), 1)
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -25,12 +39,12 @@ class Demo13091306Tests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
